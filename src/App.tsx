865961174b5272ee7e525709c4e443fce2995341
Heshan Pramuditha Dharmasena <rmheshanpramuditha@gmail.com>
2026-05-18 17:13:11 +0530

    done

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Timeline from './components/Timeline';
 import Contact from './components/Contact';
-import TerminalComponent from './components/Terminal';
 import ScrollProgress from './components/ScrollProgress';
 import BackToTop from './components/BackToTop';
 
@@ -44,7 +43,6 @@ function App() {
                 <Contact />
             </main>
 
-            <TerminalComponent />
             <BackToTop />
         </>
     );
